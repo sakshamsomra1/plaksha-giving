f023afca5b3bc9213d9f98ea86e60d49d565afdd
sakshamsomra <saksham.somra@plaksha.edu.in>
2024-02-18 19:19:15 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Areastogive from './Pages/Areastogive';
+import Home from './Pages/Home';
+import Howtogive from './Pages/Howtogive';
+
+const router = createBrowserRouter([
+{
+  path:"/",
+  element:<App />,
+  children:[
+   
+        {
+          path:"/",
+          element:<Home />
+        },
+        {
+          path:"/areas-to-give",
+          element:<Areastogive />,
+        },
+        {
+          path:"/how-to-give",
+          element:<Howtogive />,
+        }
+      ]
+    }
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
